5738d92136c2ba24ac80021c4a060006034a73bb
Brandon Doot <brandon.doot@gmail.com>
2014-02-17 13:33:24 -0500

    First crack at url shortening
    Creates a lable and maps it to a url, it doesn't autogenerate labels yet.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,17 @@
 source "https://rubygems.org"
 ruby "2.1.0"
-gem "sinatra", "1.4.4"
+gem "sinatra", "~> 1.4.4"
+gem "haml",  "~> 4.0.5"
+gem "redis", "~> 3.0.7"
+gem "data_mapper"
+
+group :production do
+    gem "pg"
+    gem "dm-postgres-adapter"
+end
+
+group :development, :test do
+    gem "sqlite3"
+    gem "dm-sqlite-adapter"
+end
 
--- web.rb
+++ web.rb
@@ -1,6 +1,45 @@
 require "sinatra" 
+require "haml"
+require "data_mapper"
 
-get "/" do 
-  "Hello world"
+DOMAIN = "http://doot.us/"
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_RED_URL'])
+end
+
+def create_link(url, label=nil)
+  label ||= url
+
+  "<a href=\"#{url}\">#{label}</a>"
+end
+
+class Link
+  include DataMapper::Resource
+
+  property :label, String, key: true
+  property :url,   String
+end
+
+DataMapper.auto_upgrade!
+
+get "/" do
+ haml :index
+end
+
+get "/:label" do
+  @link = Link.get(params[:label])
+  haml :redirect
+end
+
+post "/" do
+  @link = Link.create(params)
+  @label = "#{DOMAIN}#{@link[:label]}"
+
+  haml :index
 end
 
